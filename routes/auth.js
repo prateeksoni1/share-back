38340fd2e99195722085cb65b0bbf8a44f07e172
Proforty7 <proforty7@gmail.com>
2019-01-12 01:36:08 +0530

    fixed headers

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,9 @@ console.log("here");
 route.post("/", (req, res) => {
   console.log("called");
   console.log(req.body);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.send("ok");
 });
 
 module.exports = route;
