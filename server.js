68a99e08730de859501f32a06280fc9e95159d63
Proforty7 <proforty7@gmail.com>
2019-01-12 01:47:41 +0530

    dwd
    Signed-off-by: Proforty7 <proforty7@gmail.com>

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(bodyParser({ extended: true }));
 
 app.get("/", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.send("Welcome");
 });
 
