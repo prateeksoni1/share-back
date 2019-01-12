674a396ca1238fb48e5fa15236889427764320fa
Proforty7 <proforty7@gmail.com>
2019-01-12 20:10:36 +0530

    cors fixed
    Signed-off-by: Proforty7 <proforty7@gmail.com>

--- routes/auth.js
+++ routes/auth.js
@@ -4,8 +4,9 @@ console.log("here");
 route.post("/", (req, res) => {
   console.log("called");
   console.log(req.body);
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  // res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  // res.header("Access-Control-Max-Age", "86400");
   res.send("ok");
 });
 
